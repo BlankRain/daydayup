4c269b69a38b9c32d7a995c360b7cb57077b0fa3
BlankRain <731014656@qq.com>
2016-12-28 14:25:34 +0800

    go

--- julia/helloworld.jl
+++ julia/helloworld.jl
@@ -1,1 +1,25 @@
 println("Hello World~")
+
+println(PROGRAM_FILE); for x in ARGS; println(x); end
+# 全局常量 ARGS   PROGRAM_FILE
+
+#  ~/.juliarc.jl
+# If you have code that you want executed whenever Julia is run, you can put it in ~/.juliarc.jl:
+
+# $ echo 'println("Greetings! 你好! 안녕하세요?")' > ~/.juliarc.jl
+# $ julia
+# Greetings! 你好! 안녕하세요?
+
+
+1 # 变量
+x=1
+x=x+1 # 非函数式 非不变
+
+0x1
+0x123
+
+0b10
+
+1e10
+1e2
+(typemin(Int32), typemax(Int32))
---
+++ julia/helloworld.jl.md
@@ -0,0 +1,15 @@
+helloworld.jl.md
+
+Intn -2^(n-1) ~~ 2^(n-1) -1
+UIntn  0 ~~ 2^n -1
+
+
+
+Sys.WORD_SIZE
+ARGS   
+PROGRAM_FILE
+
+The underscore _ can be used as digit separator:
+
+julia> 10_000, 0.000_000_005, 0xdead_beef, 0b1011_0010
+(10000,5.0e-9,0xdeadbeef,0xb2)
