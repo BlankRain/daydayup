6bcc9d127dd90a1c83db393463a0e1a6b5952cc9
BlankRain <731014656@qq.com>
2017-02-20 16:19:34 +0800

    en

--- ts/word.ts
+++ ts/word.ts
@@ -1,3 +1,16 @@
 function World(){
     "World"
 }
+
+class OK{}
+interface LookGood{}
+
+class Person{
+    ok:OK
+    lookGood:LookGood
+    constructor(x:OK,y:LookGood){
+        this.ok=x;
+        this.lookGood=y;
+    }
+}
+let v=new Person("a","b");
