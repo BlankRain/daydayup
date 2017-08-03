b419d6fc7e08e810b8da6d3684774ce549fe7a7a
护群神兽之点赞狂魔 <731014656@qq.com>
2017-08-03 22:50:00 +0800

    Update md5.clj

--- clojure/md5.clj
+++ clojure/md5.clj
@@ -1,10 +1,12 @@
-(defn md5
-  "Generate a md5 checksum for the given string"
-  [token]
+(defn msg-digest
+  "Generate a key checksum for the given string"
+  [key token]
   (let [hash-bytes
-         (doto (java.security.MessageDigest/getInstance "MD5")
+         (doto (java.security.MessageDigest/getInstance key)
                (.reset)
                (.update (.getBytes token)))]
        (.toString
          (new java.math.BigInteger 1 (.digest hash-bytes)) ; Positive and the size of the number
          16)))
+(def md5 (partial msg-digest "MD5"))
+(def sha1 (partial msg-digest "sha1"))
