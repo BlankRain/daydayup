755ee3850e5bd02e809b340ee4caa9ff3590dfaf
护群神兽之点赞狂魔 <731014656@qq.com>
2018-01-16 17:48:22 +0800

    Update webapp.clj

--- clojure/webapp.clj
+++ clojure/webapp.clj
@@ -26,3 +26,145 @@ lein migratus
 (defn all-users []
   (Brain))
 
+https://github.com/metabase/toucan/blob/master/docs/defining-models.md
+https://github.com/luminus-framework/conman
+http://www.luminusweb.net/docs/services.md
+https://github.com/Day8/re-frame
+https://github.com/stuartsierra/component
+  
+re-frame
+ (ns some.core
+  (:require [reagent.core :as r]
+            [re-frame.core :as rf]
+            [secretary.core :as secretary]
+            [goog.events :as events]
+            [goog.history.EventType :as HistoryEventType]
+            [markdown.core :refer [md->html]]
+            [ajax.core :refer [GET POST]]
+            [some.ajax :refer [load-interceptors!]]
+            [some.events])
+  (:import goog.History))
+
+(defn nav-link [uri title page]
+  [:li.nav-item
+   {:class (when (= page @(rf/subscribe [:page])) "active")}
+   [:a.nav-link {:href uri} title]])
+
+(defn navbar []
+  [:nav.navbar.navbar-dark.bg-primary.navbar-expand-md
+   {:role "navigation"}
+   [:button.navbar-toggler.hidden-sm-up
+    {:type "button"
+     :data-toggle "collapse"
+     :data-target "#collapsing-navbar"} "☰"]
+   [:a.navbar-brand {:href "#/"} "killer"]
+   [:div#collapsing-navbar.collapse.navbar-collapse
+    [:ul.nav.navbar-nav.mr-auto
+     [nav-link "#/" "Home" :home]
+     [nav-link "#/about" "About" :about]]]])
+
+(defn about-page []
+  [:div.container
+   [:div.row
+    [:div.col-md-12
+     [:img {:src (str js/context "/img/warning_clojure.png")}]]]])
+
+(defn home-page []
+  [:div.container
+   (when-let [docs @(rf/subscribe [:docs])]
+     [:div.row>div.col-sm-12
+      [:div {:dangerouslySetInnerHTML
+             {:__html (md->html docs)}}]])])
+
+(def pages
+  {:home #'home-page
+   :about #'about-page})
+
+(defn page []
+  [:div
+   [navbar]
+   [(pages @(rf/subscribe [:page]))]])
+
+;; -------------------------
+;; Routes
+(secretary/set-config! :prefix "#")
+
+(secretary/defroute "/" []
+  (rf/dispatch [:set-active-page :home]))
+
+(secretary/defroute "/about" []
+  (rf/dispatch [:set-active-page :about]))
+
+;; -------------------------
+;; History
+;; must be called after routes have been defined
+(defn hook-browser-navigation! []
+  (doto (History.)
+    (events/listen
+      HistoryEventType/NAVIGATE
+      (fn [event]
+        (secretary/dispatch! (.-token event))))
+    (.setEnabled true)))
+
+;; -------------------------
+;; Initialize app
+(defn fetch-docs! []
+  (GET "/docs" {:handler #(rf/dispatch [:set-docs %])}))
+
+(defn mount-components []
+  (rf/clear-subscription-cache!)
+  (r/render [#'page] (.getElementById js/document "app")))
+
+(defn init! []
+  (rf/dispatch-sync [:initialize-db])
+  (load-interceptors!)
+  (fetch-docs!)
+  (hook-browser-navigation!)
+  (mount-components))
+
+  
+  
+  
+  events 
+  
+  (ns some.events
+  (:require [some.db :as db]
+            [re-frame.core :refer [dispatch reg-event-db reg-sub]]))
+
+;;dispatchers
+
+(reg-event-db
+  :initialize-db
+  (fn [_ _]
+    db/default-db))
+
+(reg-event-db
+  :set-active-page
+  (fn [db [_ page]]
+    (assoc db :page page)))
+
+(reg-event-db
+  :set-docs
+  (fn [db [_ docs]]
+    (assoc db :docs docs)))
+
+;;subscriptions
+
+(reg-sub
+  :page
+  (fn [db _]
+    (:page db)))
+
+(reg-sub
+  :docs
+  (fn [db _]
+    (:docs db)))
+  
+  
+  db
+  
+(ns some.db)
+
+(def default-db
+  {:page :home})
+
